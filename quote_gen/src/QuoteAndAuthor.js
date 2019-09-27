72d2023825a3a7c2d3e9ade2d3068075b6516fa9
levi <glendenning.levi@gmail.com>
2019-09-26 21:27:36 -0600

    built need to add quotes

---
+++ quote_gen/src/QuoteAndAuthor.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class QuoteAndAuthor extends React.Component {
+    render() {
+        const randomColor = this.props.displayColor();
+        const html = document.documentElement;
+        html.style.backgroundColor = randomColor;
+
+        return (
+            <div style= {{backgroundColor: "white" }} ClassName= "QuoteBox">
+            <div 
+                className= "FadeIn"
+                key= {Math.random()}
+                style= {{ color: randomColor }}
+                >
+                <h1 id= "quote"> "{this.props.quote}"</h1>
+                <h5 id= "author"> -{this.props.author ? this.props.author : "unknown"}- </h5>
+                </div>
+                <button
+                    style= {{ backgourndColor: randomColor }}
+                    id= "newquote"
+                    onClick= {this.props.handleClick}
+                > New quote
+                </button>
+            </div>
+        );
+    }
+}
+
+export default QuoteAndAuthor;
---
+++ quote_gen/src/QuotesDatabase.js
@@ -0,0 +1,41 @@
+const quotes= [
+    {
+        "quote": "Oh no, That's a fucking wire brush to my hemorrhoids",
+        "author": "Ben Caffery"
+    },{
+        "quote": "A suck-up isn't going to fix a fuck-up",
+        "author": "Selina Meyer"
+    },{
+        "quote": "GO, period. Fuck, period. Yourself, EXCLAMATION POINT!",
+        "author": "Selina Meyer"
+    },{
+        "quote": "If there is any dirty trick I cannot stand, it is honesty",
+        "author": "Any Brookheimer"
+    },{
+        "quote": "God, there's so many people in here. It's like a Mormon orgy",
+        "author": "Selina Meyer"
+    },{
+        "quote": "Uzbekistan is between Turkmenistan and Icouldgiveafuckistan",
+        "author": "Ben Cafferty"
+    },{
+        "quote": "",
+        "author": "Selina Meyer & Ben Caffery"
+    },{
+        "quote": "",
+        "author": ""
+    },{
+        "quote": "",
+        "author": ""
+    },{
+        "quote": "",
+        "author": ""
+    },{
+        "quote": "",
+        "author": ""
+    },{
+        "quote": "",
+        "author": ""
+    }
+]
+
+export default quotes
